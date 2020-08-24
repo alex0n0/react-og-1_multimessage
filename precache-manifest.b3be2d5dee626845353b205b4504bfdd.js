f33fd8f878ca8e057e8aec1806a6c1e692fa6170
alex0n0 <42526086+alex0n0@users.noreply.github.com>
2020-08-24 15:29:12 +1000

    Updates

---
+++ precache-manifest.b3be2d5dee626845353b205b4504bfdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b0e485ed787b18bfb36195f4612aea",
+    "url": "/multimessage/index.html"
+  },
+  {
+    "revision": "3492446ed5bfaff4fa57",
+    "url": "/multimessage/static/css/main.fb3faf3a.chunk.css"
+  },
+  {
+    "revision": "dbdf1377836324fb48d7",
+    "url": "/multimessage/static/js/2.ec45182f.chunk.js"
+  },
+  {
+    "revision": "f3a6a51dd260c4f924bf796e7b33f546",
+    "url": "/multimessage/static/js/2.ec45182f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3492446ed5bfaff4fa57",
+    "url": "/multimessage/static/js/main.c72cd8d7.chunk.js"
+  },
+  {
+    "revision": "c603966a61e28f8792dd",
+    "url": "/multimessage/static/js/runtime-main.7629a806.js"
+  }
+]);
